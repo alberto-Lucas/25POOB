8d1812c0a8856d05c618c77ede0838ded4ecc568
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-08-27 20:43:46 -0300

    TestePOO

---
+++ TestePOO/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestePOO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Instanciar a nossa classe Pessoa
+            //Apenas instanciando
+            //(ou seja alocando a classe pessoa me memoria)
+            //Composto por
+            //Tipode de Dados: Pessoa(Do mesmo tipo da classe)
+            //Variavel: pessoa (geralmente mesmo nome da classe)
+            //=: atribuir o ponteiro da memoria a variavel
+            //new: ação de alocar na memoria
+            //Construtor da classe: Pessoa() (o mesmo nome do tipo de dados)
+            Pessoa pessoa = new Pessoa();
+
+            //Após instanciado, conseguimos acessar
+            //o conteudo da classe q é permetido(public)
+            //seguindo o exemplo
+            //vamos definir um valor para o atributo Nome
+            //Para isso, é preciso usar a variavel com a instancia
+            //utilizar o sinal de ponto . para acessar as inforamções
+            //disponiveis, e utilizar o atributo ou método desejado
+            pessoa.Nome = "Lucas";
+            pessoa.CPF = "5165456";
+            pessoa.DtNascimento = DateTime.Now.Date; //data atual
+
+            //Criar ou classe pessoa
+            Pessoa aluno = new Pessoa();
+            aluno.Nome = "João";
+            aluno.CPF = "546545";
+            aluno.DtNascimento = DateTime.Now.Date;
+
+            //Exibir os dados usando o método
+            ExibirClasse(pessoa);
+            ExibirClasse(aluno);
+
+            pessoa.MsgCPFNome();
+            aluno.MsgCPFNome();
+        }
+
+        //Criar um para exibir dados da classe Pessoa
+        void ExibirClasse(Pessoa classe)
+        {
+            MessageBox.Show(
+                "Nome: " + classe.Nome + Environment.NewLine +
+                "CPF: " + classe.CPF + Environment.NewLine +
+                "DtNascimento: " + classe.DtNascimento.ToShortDateString());
+        }
+    }
+}
---
+++ TestePOO/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TestePOO
+{
+    //Toda classe precisa ser public
+    public class Pessoa
+    {
+        //Atributo
+        //é composto por
+        //Nivel de acesso: Pulic
+        //Tipo de dados: string
+        //Nome do atributo: Nome
+        //Configuração
+        //get: leitura
+        //set: gravação
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DtNascimento { get; set; }
+
+        //Método para exibir o CPF concatenado com o Nome
+        //EX: CPF - NOME
+        //Obrigatoria para eu acessar o método
+        //fora da classe, ele precisa ser PUBLIC
+        public void MsgCPFNome()
+        {
+            MessageBox.Show(CPF + " - " + Nome);
+        }
+    }
+}
